395abe2f61bb9bbd5c223ad12bc0718a91e6de20
lepidodendronnnn <alexandrevnas@gmail.com>
2024-10-30 12:40:15 +0300

    pt 1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,28 @@
 package org.example;
 
 import com.fastcgi.FCGIInterface;
+
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 class Main {
     static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     static int r;
+    static int x;
+    static float y;
     private static final String RESULT_JSON = """
             {
+                "x":"%s",
+                "y": "%s",
+                "r": "%s",
                 "answer": %b,
                 "executionTime": "%s",
                 "serverTime": "%s"
@@ -35,27 +44,48 @@ class Main {
         """;
     private static final String ERROR_JSON = """
         {
+            "x":"%s",
+            "y": "%s",
+            "r": "%s",
             "reason": "%s"
         }
         """;
 
     public static void main(String[] args) throws IOException {
-        var fcgiInterface = new FCGIInterface();
+        FCGIInterface fcgiInterface = new FCGIInterface();
         while (fcgiInterface.FCGIaccept() >= 0) {
             long startTime = System.nanoTime();
+            boolean correctX = false;
+            boolean correctY = false;
+            boolean correctR = false;
             try {
                 String body = readRequestBody();
                 Map<String, String> params = parse(body);
 
-                float x = Float.parseFloat(params.get("x"));
-                int y = Integer.parseInt(params.get("y"));
-                r = Integer.parseInt(params.get("r"));
+                if (params.containsKey("x")) {
+                    x = Integer.parseInt(params.get("x"));
+                    correctX = true;
+                }
+                if (params.containsKey("y")) {
+                    BigDecimal yValue = new BigDecimal(params.get("y"));
+                    y = yValue.setScale(2, RoundingMode.HALF_UP).floatValue();
+                    if (y > -5 && y < 3) {
+                        correctY = true;
+                    }
+                }
+                if (params.containsKey("r")) {
+                    r = Integer.parseInt(params.get("r"));
+                    correctR = true;
+                }
 
-                boolean insideRectangle = isInsideRectangle(x, y, r, r, r);
-                boolean insidePolygon = isInsidePolygon(x, y);
-                boolean insidePath = isInsidePath(x, y);
+                if (!(correctY && correctX && correctR)) {
+                    throw new Exception("incorrect parameters");
+                }
+                boolean insideRectangle = isInsidePolygon(x, y, r);
+                boolean insidePolygon = isInsideRectangle(x, y, r);
+                boolean insidePath = isInsidePath(x, y, r);
 
-                var json = String.format(RESULT_JSON, insideRectangle || insidePolygon || insidePath, System.nanoTime() - startTime, LocalDateTime.now().format(formatter));
+                var json = String.format(RESULT_JSON, x, y, r, insideRectangle || insidePolygon || insidePath, System.nanoTime() - startTime, LocalDateTime.now().format(formatter));
                 var responseBody = json.trim();
                 var response = String.format(HTTP_RESPONSE, responseBody.getBytes(StandardCharsets.UTF_8).length, responseBody);
                 try {
@@ -66,11 +96,31 @@ class Main {
                 }
 
             } catch (Exception ex) {
-                var json = String.format(ERROR_JSON, ex.getMessage());
+                Function<Boolean, String> xCheck = status -> {
+                    if (status){
+                        return String.valueOf(x);
+                    }
+                    return "incorrect Value";
+                };
+                Function<Boolean, String> yCheck = status -> {
+                    if (status){
+                        return String.valueOf(y);
+                    }
+                    return "incorrect Value";
+                };
+                Function<Boolean, String> rCheck = status -> {
+                    if (status){
+                        return String.valueOf(r);
+                    }
+                    return "incorrect Value";
+                };
+                var json = String.format(ERROR_JSON, xCheck.apply(correctX), yCheck.apply(correctY), rCheck.apply(correctR), ex.getMessage());
                 var responseBody = json.trim();
                 var response = String.format(HTTP_ERROR, responseBody.getBytes(StandardCharsets.UTF_8).length, responseBody);
                 FCGIInterface.request.outStream.write(response.getBytes(StandardCharsets.UTF_8));
                 FCGIInterface.request.outStream.flush();
+
+                //как работают потоки в джаве
             }
         }
     }
@@ -86,12 +136,13 @@ class Main {
             buffer.clear();
             return new String(requestBodyRaw, StandardCharsets.UTF_8);
         } catch (NullPointerException e){
-            return "";
+           return e.getMessage();
         }
     }
 
     private static Map<String, String> parse(String queryParams){
         Map<String, String> params = new HashMap<>();
+
         if (queryParams != null && !queryParams.isEmpty()) {
             String[] pairs = queryParams.split("&");
             for (String pair : pairs) {
@@ -100,54 +151,19 @@ class Main {
                     params.put(keyValue[0], keyValue[1]);
                 }
             }
-
-        } else {
-            return params;
         }
-
         return params;
     }
-    private static boolean isInsideRectangle(double px, double py, double rectY, double width, double height) {
-        return (px >= (double) 0 && px <= (double) 0 + width) && (py >= rectY && py <= rectY + height);
+    private static boolean isInsidePolygon(double x, double y, double r) {
+        return x <= r && y <= r && x >= 0 && y >= 0;
     }
 
-    private static boolean isInsidePolygon(double px, double py) {
-        // Определим вершины многоугольника
-        double[][] polygon = {
-                {0, (double) -r /2},
-                {0, 0},
-                {r, 0}
-        };
-
-        int n = polygon.length;
-        boolean inside = false;
-
-        for (int i = 0, j = n - 1; i < n; j = i++) {
-            double xi = polygon[i][0], yi = polygon[i][1];
-            double xj = polygon[j][0], yj = polygon[j][1];
-
-            boolean intersect = ((yi > py) != (yj > py)) && (px < (xj - xi) * (py - yi) / (yj - yi) + xi);
-            if (intersect) {
-                inside = !inside;
-            }
-        }
-
-        return inside;
+    private static boolean isInsideRectangle(double x, double y, double r) {
+        return y >= 0.5*x - r/2 && y <= 0 && y >= -r/2 && x >= 0 && x <= r;
     }
 
-    private static boolean isInsidePath(double x, double y) {
-        double centerX = 0;
-        double centerY = 0;
+    private static boolean isInsidePath(double x, double y, double r) {
         double radius = r / 2;
-        if (x <= centerX && y <= centerY && x >= (centerX - radius) && y >= (centerY - radius)) {
-            double distanceSquared = Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2);
-            if (distanceSquared <= Math.pow(radius, 2)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
+        return x <= 0 && y <= 0 && x >= (0 - radius) && y >= (0 - radius) && Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(radius, 2);
     }
 }
